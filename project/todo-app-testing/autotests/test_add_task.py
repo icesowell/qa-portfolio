c77c17e5032978ad6666a0516392f91c2e700d23
Nikita <108310900+icesowell@users.noreply.github.com>
2025-04-15 17:07:28 +0300

    Update test_add_task.py

--- project/todo-app-testing/autotests/test_add_task.py
+++ project/todo-app-testing/autotests/test_add_task.py
@@ -1,1 +1,20 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+URL = "https://todomvc.com/examples/react/dist/"
+
+def test_add_task(browser):
+    browser.get(URL)
+    input_field = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "new-todo"))
+    )
+    input_field.send_keys("Test Task")
+    input_field.send_keys(Keys.ENTER)
+
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".todo-list li"))
+    )
+    todos = browser.find_elements(By.CSS_SELECTOR, ".todo-list li")
+    assert any("Test Task" in todo.text for todo in todos)
