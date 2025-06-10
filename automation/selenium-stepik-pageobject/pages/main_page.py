6c68384b92ee566f193950d24e9dad50c575b801
Nikita <icesowell@gmail.com>
2025-06-10 21:06:32 +0300

    Изменил страницы логина, добавили переходы между страницами

--- automation/selenium-stepik-pageobject/pages/main_page.py
+++ automation/selenium-stepik-pageobject/pages/main_page.py
@@ -1,12 +1,14 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from pages.login_page import LoginPage
+from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
     
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.ID, 'login_link')
-        login_link.click()
-    
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
     def should_be_login_link(self):
         assert self.is_element_present(By.ID, 'login_link'), "На сайте нет ссылки на логин"
--- automation/selenium-stepik-pageobject/test_main_page.py
+++ automation/selenium-stepik-pageobject/test_main_page.py
@@ -7,9 +7,11 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
 
     page.open() #open page
-    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    #login_page = page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
+    
 
-    page.should_be_login_link()
+    login_page.should_be_login_page()
 
 def test_guest_can_see_login_from(browser):
 
