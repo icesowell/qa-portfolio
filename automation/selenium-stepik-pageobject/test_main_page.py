4879bf39521e49d80c80381c74f17a7c42df867b
Nikita <icesowell@gmail.com>
2025-06-14 16:32:42 +0300

    Добавили файл pytest.ini прописали в него информацию о метках, объединили тестовые методы (для логина) в общий класс и дали ему метку

--- automation/selenium-stepik-pageobject/test_main_page.py
+++ automation/selenium-stepik-pageobject/test_main_page.py
@@ -1,39 +1,42 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
-import time
+import pytest
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+                 
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
 
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
 
-    page.open() #open page
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
+        page.open() #open page
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
 
-    login_page.should_be_login_page()
+        login_page.should_be_login_page()
 
-def test_guest_can_see_login_from(browser):
+    def test_guest_can_see_login_from(self, browser):
 
-    login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+        login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
 
-    login = LoginPage(browser, login_link)
+        login = LoginPage(browser, login_link)
 
-    login.open()
+        login.open()
 
-    login.should_be_login_url()
+        login.should_be_login_url()
 
-    login.should_be_login_form()
+        login.should_be_login_form()
 
-def test_guest_can_see_register_from(browser):
-    register_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    def test_guest_can_see_register_from(self, browser):
+        register_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
 
-    register = LoginPage(browser, register_link)
+        register = LoginPage(browser, register_link)
 
-    register.open()
+        register.open()
 
-    register.should_be_register_form()
+        register.should_be_register_form()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -48,4 +51,4 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 
     basket_page.should_be_empty_text_basket()
     basket_page.should_be_not_include_items()
-    time.sleep(5)
+
